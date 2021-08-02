dab061b590f104fda60d657bc3f774f6789cdcc1
Mitch Dolby <mitchdolby1@gmail.com>
2021-08-02 13:26:43 -0400

    'debugging'

--- census_api/census_api_venv/census_api_bg.py
+++ census_api/census_api_venv/census_api_bg.py
@@ -10,12 +10,11 @@ def main():
     parser = ArgumentParser()
     parser.add_argument('state', help='2 letter abbreviation of state/territory of choice', type=str)
     parser.add_argument('--overwrite', help='overwrite raw file or save as new file', action='store_true') 
-    args, unknown = parser.parse_known_args() 
-    if args.state not in states and args.state.lower() not in states:
+    args, unknown = parser.parse_known_args()
+    if args.state.upper() not in states:
         print('Abbreviation is invalid')
         exit(0)
-    state_fips = states[args.state]
-    response = requests.get("https://api.census.gov/data/2019/acs/acs5?get=NAME,GEO_ID,B19013_001E&for=block group:*&in=state:{}&in=county:*&in=tract:*&key={}".format(state_fips, api_key))
+    response = requests.get("https://api.census.gov/data/2019/acs/acs5?get=NAME,GEO_ID,B19013_001E&for=block group:*&in=state:{}&in=county:*&in=tract:*&key={}".format(states[args.state.upper()], api_key))
     print(response.status_code)
     result = json.loads(response.text)
     print(result)
