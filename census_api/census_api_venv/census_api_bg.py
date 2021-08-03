502e54df87c6f90f5901266da6ca8b860f8b2763
Mitch Dolby <mitchdolby1@gmail.com>
2021-08-03 00:02:34 -0400

    added a clean function and debugged

--- census_api/census_api_venv/census_api_bg.py
+++ census_api/census_api_venv/census_api_bg.py
@@ -6,6 +6,15 @@ from argparse import ArgumentParser
 from config import api_key
 from states import states
 
+def clean(df):
+    df.columns = ['index', 'NAME', 'GEO_ID', 'INCOME', 'state', 'county', 'tract', 'block group']
+    geoid = df['GEO_ID']
+    df[['US_Code', 'GEO_ID']] = geoid.str.split("US", n=1, expand=True)
+    del df['US_Code']
+    df.drop(df.index[0], inplace=True)
+    df = df[df.INCOME != '-666666666']
+    return df
+
 def main():
     parser = ArgumentParser()
     parser.add_argument('state', help='2 letter abbreviation of state/territory of choice', type=str)
@@ -17,16 +26,9 @@ def main():
     response = requests.get("https://api.census.gov/data/2019/acs/acs5?get=NAME,GEO_ID,B19013_001E&for=block group:*&in=state:{}&in=county:*&in=tract:*&key={}".format(states[args.state.upper()], api_key))
     print(response.status_code)
     result = json.loads(response.text)
-    print(result)
     pd.DataFrame(result).to_csv(f'income_bg_{args.state.upper()}.csv')
     df = pd.read_csv(f'income_bg_{args.state.upper()}.csv')
-    df.columns
-    df.columns = ['index', 'NAME', 'GEO_ID', 'INCOME', 'state', 'county', 'tract', 'block_group']
-    GEO_ID = df['GEO_ID']
-    df[['US_Code', 'GEO_ID']] = GEO_ID.str.split("US", n=1, expand=True)
-    del df['US_Code']
-    df = df.drop(df.index[0])
-    del df['index']
+    df = clean(df)
     if args.overwrite:
         df.to_csv(f'income_bg_{args.state.upper()}.csv')
     else:
