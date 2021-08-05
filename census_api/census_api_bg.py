a4087e09271d1e65e90d117c1d2b0d4a3a408bd9
Mitch Dolby <mitchdolby1@gmail.com>
2021-08-05 07:39:36 -0400

    added docstrings

--- census_api/census_api_bg.py
+++ census_api/census_api_bg.py
@@ -1,4 +1,3 @@
-#import the following libraries and api key from config script
 import requests
 import json
 import pandas as pd
@@ -7,6 +6,8 @@ from config import api_key
 from states import states
 
 def clean(df):
+    """This clean function drops an index column, removes the unusable half of the GEO ID, renames the
+    columns, gives the nulls no value from -666666666, and drops an unneeded row"""
     del df['Unnamed: 0']
     df['1'] = df['1'].str.replace("1500000US", "")
     df.rename(columns={'1': 'GEO_ID'})
@@ -16,6 +17,12 @@ def clean(df):
     return df
 
 def main():
+    """The 3 current arguments require the user to input the state/territory code, their desired variables,
+    and the option to overwrite their inital raw file to prevent cluttering. After parsing the arguments
+    it will then run a get request using the 2 requirements and the API key from the config file as format
+    variables. This data will save as a csv into the directory with the inital raw data, which will then
+    run through the 'clean' function as a dataframe. The new cleaned file will either save as a separate
+    csv into the directory or will overwrite the raw file."""
     parser = ArgumentParser()
     parser.add_argument('state', help="2 letter abbreviation of state (DC and Puerto Rico included) of choice", type=str)
     parser.add_argument('var', help="Find a variable from the ACS 5-year detailed tables and enter the variable"+
@@ -27,8 +34,9 @@ def main():
         print('Abbreviation is invalid')
         exit(0)
     response = requests.get(f"https://api.census.gov/data/2019/acs/acs5?get=NAME,GEO_ID,{args.var}&for=block%20group:*&in=state:{states[args.state.upper()]}&in=county:*&in=tract:*&key={api_key}")
-    #print('Status Code: ', response.status_code)
+    #('Status Code: ', response.status_code) If the status code is 400 or any variant, print that it failed
     result = json.loads(response.text)
+    #write function that saves the data into a data folder, creates one smaller folder for each state
     pd.DataFrame(result).to_csv(f'income_bg_{args.state.upper()}.csv')
     df = pd.read_csv(f'income_bg_{args.state.upper()}.csv')
     df = clean(df)
